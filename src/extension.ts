3b35e6080e60f3b02cbe0364d145576a3e68ea9d
Swapnil Tripathi <nswapt@gmail.com>
2021-03-24 11:25:09 +0530

    registering command

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,27 @@ import { CodeJsonTreeProvider } from './providers/code-json-tree-provider';
 export function activate(_context: vscode.ExtensionContext) {
   const provider = new CodeJsonTreeProvider();
 
+  const commandHandler = () => {
+    provider.refreshTree();
+  };
+
+  const commandHandlerView = () => {
+  };
+
   _context.subscriptions.push(
     vscode.window.onDidChangeActiveTextEditor(() => provider.onDidChangeActiveTextEditor()),
-    vscode.workspace.onDidChangeTextDocument(e => provider.onDidChangeTextDocument(e))
+    vscode.workspace.onDidChangeTextDocument(e => provider.onDidChangeTextDocument(e)),
+    vscode.commands.registerCommand('code-json-editor.view', commandHandler),
+    vscode.commands.registerCommand('code-json-editor.refresh', commandHandlerView)
+
   );
 
   vscode.window.createTreeView('code-json-editor', {
     treeDataProvider: provider,
     showCollapseAll: true,
   });
+
+  
 
   provider.refreshTree();
   console.log('Congratulations, your extension "code-json-editor" is now ready!');
