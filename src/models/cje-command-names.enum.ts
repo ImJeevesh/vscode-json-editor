f24587f2adabdd012b4923a3d4b34fa5d19f3b13
Jeevesh <jrawat2009@gmail.com>
2021-03-31 11:38:49 +0530

    feat: add jump and copy to tree item actions

--- src/models/cje-command-names.enum.ts
+++ src/models/cje-command-names.enum.ts
@@ -1,4 +1,7 @@
 export enum CJECommandNames {
   treeActive = 'code-json-editor.tree-active',
   treeItemSelection = 'code-json-editor.tree-item-selection',
+  highlightValue = 'code-json-editor.highlight-value',
+  jumpEnd = 'code-json-editor.jump-end',
+  copyValueToClipboard = 'code-json-editor.copy-value-to-clipboard',
 }
